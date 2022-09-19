1ffd89025fdbbead9f0eb2e8a07aa17cb1a0e9ae
Lucas Henrique <lukas.paes18@gmail.com>
2022-09-19 11:41:31 -0300

    feature: finalizado busca de dados no facebook

--- main.py
+++ main.py
@@ -140,12 +140,32 @@ def buscarDadosFacebook():
   
   driver.get(url)
   
-  for a in range(0,10):
-    sleep(2)
+  for a in range(0,1):
+    sleep(4)
     driver.execute_script('window.scrollTo(0, document.body.scrollHeight+'+str(100*a)+');')
-    sleep(0.40)
+    sleep(4)
   
+  result = driver.page_source
   
+  soup = BeautifulSoup(result, "lxml")
+
+  items = soup.find_all("a", attrs={"class":"qi72231t nu7423ey n3hqoq4p r86q59rh b3qcqh3k fq87ekyn bdao358l fsf7x5fv rse6dlih s5oniofx m8h3af8h l7ghb35v kjdc1dyq kmwttqpk srn514ro oxkhqvkx rl78xhln nch0832m cr00lzj9 rn8ck1ys s3jn8y49 icdlwmnq jxuftiz4 l3ldwz01"})
+  
+  for item in items:
+    try:
+      valorTelefone = item.find_all("span", class_="gvxzyvdx aeinzg81 t7p7dqev gh25dzvf tb6i94ri gupuyl1y i2onq4tn b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas f5mw3jnl hxfwr5lz hpj0pwwo sggt6rq5 innypi6y pbevjfx6")[0].contents[0]
+      
+      
+      nomeTelefone = item.find_all("span",class_="b6ax4al1 lq84ybu9 hf30pyar om3e55n1")[0].contents[0]
+      
+      
+      urlTelefone = "https://www.facebook.com"+item["href"]
+      
+      print(urlTelefone)
+      
+      
+    except:
+      print("erro")
   
   
 buscarDadosFacebook()
