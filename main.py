7b58824e710dda39033f5d12ae4b13190d0f2370
Lucas Henrique <lukas.paes18@gmail.com>
2022-09-21 18:20:36 -0300

    feat: minerando

--- main.py
+++ main.py
@@ -131,45 +131,6 @@ def buscarDadosMercadoLivre(pages = 2, regiao = "GR"):
       
 
 
-def buscarDadosFacebook(manyTimeScroll = 2):
-  url = facebook_link
-  
-  options = Options()
-  options.add_argument("start-maximized")
-  
-
-  driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-  
-  driver.get(url)
-  
-  for a in range(0,manyTimeScroll):
-    sleep(2)
-    driver.execute_script('window.scrollTo(0, document.body.scrollHeight+'+str(100*a)+');')
-    sleep(2)
-  
-  result = driver.page_source
-  
-  soup = BeautifulSoup(result, "lxml")
-
-  items = soup.find_all("a", attrs={"class":"qi72231t nu7423ey n3hqoq4p r86q59rh b3qcqh3k fq87ekyn bdao358l fsf7x5fv rse6dlih s5oniofx m8h3af8h l7ghb35v kjdc1dyq kmwttqpk srn514ro oxkhqvkx rl78xhln nch0832m cr00lzj9 rn8ck1ys s3jn8y49 icdlwmnq jxuftiz4 l3ldwz01"})
-  
-  for item in items:
-    try:
-      valorTelefone = item.find_all("span", class_="gvxzyvdx aeinzg81 t7p7dqev gh25dzvf tb6i94ri gupuyl1y i2onq4tn b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas f5mw3jnl hxfwr5lz hpj0pwwo sggt6rq5 innypi6y pbevjfx6")[0].contents[0]
-      
-      nomeTelefone = item.find_all("span",class_="b6ax4al1 lq84ybu9 hf30pyar om3e55n1")[0].contents[0]
-      
-      urlTelefone = "https://www.facebook.com"+item["href"]
-      
-      json = {"nome":nomeTelefone, "valor":valorTelefone*cotacao_dolar, "link":urlTelefone  }
-      
-      listaJson.append(json)
-      
-    except:
-      print("erro")
-
-
-
 def buscarDadosFacebookMiami(manyTimeScroll = 2):
   url = facebook_link_miami
   
@@ -192,11 +153,6 @@ def buscarDadosFacebookMiami(manyTimeScroll = 2):
 
   items = soup.find_all("a", attrs={"class":"qi72231t nu7423ey n3hqoq4p r86q59rh b3qcqh3k fq87ekyn bdao358l fsf7x5fv rse6dlih s5oniofx m8h3af8h l7ghb35v kjdc1dyq kmwttqpk srn514ro oxkhqvkx rl78xhln nch0832m cr00lzj9 rn8ck1ys s3jn8y49 icdlwmnq jxuftiz4 l3ldwz01"})
   
-  
-  
-
-  
-  
   for item in items:
     try:
       valorTelefone = item.find_all("span", class_="gvxzyvdx aeinzg81 t7p7dqev gh25dzvf tb6i94ri gupuyl1y i2onq4tn b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas f5mw3jnl hxfwr5lz hpj0pwwo sggt6rq5 innypi6y pbevjfx6")[0].contents[0]
@@ -216,11 +172,11 @@ def buscarDadosFacebookMiami(manyTimeScroll = 2):
       print("erro")
   
   
-buscarDadosFacebookMiami(manyTimeScroll = 2)
+buscarDadosFacebookMiami(manyTimeScroll = 30)
   
-filterText = "iPhone 5|iPhone 6|iPhone 7|iPhone SE|iPhone 4|Vitrine|Para Retirada De Peças"  
+filterText = "iPhone 5|iPhone 6|iPhone 7|iPhone SE|iPhone 4|Vitrine|Para Retirada De Peças|iPhone 8"  
 filterColumnName = "nome"
-filterPrice = 100
+filterPrice = 400
 
 req_dolar = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
 
@@ -228,14 +184,16 @@ requisicao_dic = req_dolar.json()
 
 cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
 
+cotacao_dolar = round(float(cotacao_dolar))
+
 df = pd.DataFrame(listaJson)
 
 df = df[df[filterColumnName].str.contains(filterText) == False]
 
-df.loc[(df.valor > 100), 'valor'] = df.valor * cotacao_dolar
+df.valor = df.valor * cotacao_dolar
 
 df.drop(df[df['valor'] < filterPrice].index, inplace = True)
 
-print(df)
-
 df.to_excel("telefone.xlsx")
+
+print(df)
